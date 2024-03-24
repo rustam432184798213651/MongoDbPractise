40284f2f07feda934e9ecfe934434db70634838a
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2024-03-24 18:55:41 +0300

    Add interaction with database

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
 var path = require('path');
-
+const mongoose = require("mongoose")
+mongoose.connect('mongodb://127.0.0.1:27017/new').
+  catch(error => console.log(error));
+mongoose.connection.on('connected', () => console.log('connected'));
 const app = express();
 const PORT = 443;
+const PersonSchema = new mongoose.Schema({
+  FirstName: String,
+  LastName: String
+});
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,11 +28,30 @@ const storage = multer.diskStorage({
   })
   
   const upload = multer({storage: storage})
+app.get('/NikitaIsTheBest', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Nikita is the the best');
+})
+app.get('/:fname', (req, res) => {
+  
+  const human = mongoose.model('human', PersonSchema);
+  human.findOne({ 'FirstName': req.params.fname}).then((docs)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({FirstName: docs.FirstName, LastName: docs.LastName}));
+  })
+  .catch((err)=>{
+      res.setHeader('Content-Type', 'text/html');
+      res.sendFile(__dirname + '/does_not_exist.html');
+  });
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+  //res.sendFile(__dirname + '/index.html'); 
 });
 
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(__dirname + '/index.html');
+})
 function return_response(fileName, res){
     fs.readFile("uploads\\" + fileName, 'utf8', function(err, data) {
         if (err) throw err;
@@ -39,11 +70,16 @@ function return_response(fileName, res){
       });
 }
 
-app.post('/api/upload', upload.single('file'), (req, res) => {
+
+
+app.post('/api/upload',  (req, res) => {
     // const pathToFile = "uploads/" + req.file.filename;
     // res.setHeader('Content-Type', "application/json");
     // res.send(JSON.stringify(get_dictionary(pathToFile)));
-    return_response(req.file.filename, res);
+    //mongoose.users.findOne({"username" : {$regex : "son"}});
+
+    const human = mongoose.model('People', PersonSchema);
+    
 });
 
 // Integration with server
@@ -51,3 +87,17 @@ app.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`)
 });
 
+function insertDataToMongoDB(){
+    const data = document.getElementById('SubmitData')
+}
+
+app.post("/", (req, res) => {
+  const first_name = req.body.fname;
+  const  last_name = req.body.textarea;
+  console.log("First name: " + first_name);
+  console.log("Last name: " + last_name);
+  const human = mongoose.model('human', PersonSchema);
+  const current = new human({ FirstName: first_name, LastName: last_name});
+  current.save();
+  res.send("Data received");
+})
